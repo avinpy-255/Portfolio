27b47029784f830f6ebc1471c8eee83c34be092d
Avinash <avinashchowdhury81@gmail.com>
2024-07-07 23:59:21 +0530

    updated whole site

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function toggleMobileMenu() {
     document.getElementById("menu").classList.toggle("active");
 }
+
+for (let i = 0; i < fromInput.length; i ++) {
+    fromInputs[i].addEventListener("input", function()
+    {
+        if (FormData.checkValidity())
+    }
+}
